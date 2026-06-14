c31ca19e7e8a44dee31c30b5a862bdae7548e761
Dev-developer69 <devansh123maheshwari123@gmail.com>
2026-06-14 21:24:25 +0530

    Update home_base_layout.py

--- src/ui/home_base_layout.py
+++ src/ui/home_base_layout.py
@@ -40,7 +40,7 @@ def home_layout():
             @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
 
             .stApp {
-                background: #FFF9E6 !important;
+                background: yellow !important;
             }
 
             #MainMenu, header {
