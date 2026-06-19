8c223b21426ad5dc1f352032cc593128f9abc30c
Dev-developer69 <devansh123maheshwari123@gmail.com>
2026-06-19 09:33:02 +0530

    Update vehicle_0303.py

--- src/vehicle_records/vehicle_0303.py
+++ src/vehicle_records/vehicle_0303.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.ui.excel_format import editable_grid, driver_salary, expenses
+from src.ui.excel_format import editable_grid, driver_salary, expenses, diesel_view
 from src.ui.home_base_layout import background,home_layout
 
 def page_0303():
@@ -41,9 +41,14 @@ def expense_0303():
 
     if st.button('Driver Salary',type='tertiary', width='stretch', icon=':material/home:'):
         st.session_state['expense_tab'] = 'driver'
+    
+    if st.button('Diesel View', type='tertiary', width='stretch', icon=':material/local_gas_station:'):
+        st.session_state['expense_tab'] = 'diesel'
 
 
     if st.session_state['expense_tab'] == 'vehicle':
         expenses(bus_number='0303')
     elif st.session_state['expense_tab'] == 'driver':
         driver_salary(bus_number='0303')
+    elif st.session_state['expense_tab'] == 'diesel':
+        diesel_view(bus_number='0303')
