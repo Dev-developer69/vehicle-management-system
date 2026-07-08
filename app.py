5a736cee37318ffb60c576b50e989a9e1a6f3467
Dev-developer69 <devansh123maheshwari123@gmail.com>
2026-07-08 05:37:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(
     page_title="Vehicle Management",
     page_icon="🚌",
     layout="wide",
-    initial_sidebar_state="expanded"
+  #  initial_sidebar_state="expanded"
 )
 
 from src.screens.home_page import home_page
