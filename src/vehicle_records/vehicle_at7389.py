e2ac681300a478f51c18b18ea020497d091ea4fb
Dev-developer69 <devansh123maheshwari123@gmail.com>
2026-08-15 11:23:58 +0530

    Create vehicle_at7389.py

---
+++ src/vehicle_records/vehicle_at7389.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from src.ui.excel_format import editable_grid, expenses, driver_salary, diesel_view
+from src.ui.home_base_layout import background, home_layout
+
+
+def page_AT7389():
+    home_layout()
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Home page',type='secondary', width='stretch', icon=':material/home:', shortcut='control+backspace'):
+            st.session_state['login_state']= None
+            st.rerun()
+
+    with col2:
+        if st.button('Back page', type='primary', width='stretch', icon=':material/home:', shortcut='control+enter'):
+            st.session_state['login_state']= 'vehicle_record'
+            st.rerun()
+
+    editable_grid(bus_number='AT7389')
+
+
+def expense_AT7389():
+
+    background()
+    st.header("Expense page AT7389 ", text_alignment='center')
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button('Home page', type='secondary', width='stretch', icon=':material/home:', shortcut='control+backspace'):
+            st.session_state['login_state']= None
+            st.rerun()
+
+    with col2:
+        if st.button('Back page',type='primary', width='stretch', icon=':material/home:', shortcut='control+enter'):
+            st.session_state['login_state']= 'expenses'
+            st.rerun()
+
+    if 'expense_tab' not in st.session_state:
+        st.session_state['expense_tab'] = None
+
+    if st.button('Vehicle Expense',type='tertiary', width='stretch', icon=':material/home:'):
+        st.session_state['expense_tab'] = 'vehicle'
+
+    if st.button('Driver Salary',type='tertiary', width='stretch', icon=':material/home:'):
+        st.session_state['expense_tab'] = 'driver'
+    
+    if st.button('Diesel View', type='tertiary', width='stretch', icon=':material/local_gas_station:'):
+        st.session_state['expense_tab'] = 'diesel'
+
+
+    if st.session_state['expense_tab'] == 'vehicle':
+        expenses(bus_number='AT7389')
+    elif st.session_state['expense_tab'] == 'driver':
+        driver_salary(bus_number='AT7389')
+    elif st.session_state['expense_tab'] == 'diesel':
+        diesel_view(bus_number='AT7389')
