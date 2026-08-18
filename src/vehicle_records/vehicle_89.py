06282482d1fab09e8904367c930186c98487fb92
Dev-developer69 <devansh123maheshwari123@gmail.com>
2026-08-18 08:55:34 +0530

    Update vehicle_89.py

--- src/vehicle_records/vehicle_89.py
+++ src/vehicle_records/vehicle_89.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from src.ui.excel_format import editable_grid, expenses, driver_salary, diesel_view
-from src.ui.home_base_layout import background, home_layout
+from src.ui.home_base_layout import background, home_layout, page_bg
 
 
 def page_7389():
     home_layout()
+    page_bg("#1F4A2C")  # Forest Green
     col1, col2 = st.columns(2)
     with col1:
         if st.button('Home page',type='secondary', width='stretch', icon=':material/home:', shortcut='control+backspace'):
